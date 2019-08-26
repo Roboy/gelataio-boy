73137394dd6183b75b4321a6fa23452dd0277171
Arne Sachtler <arne.sachtler@gmail.com>
2019-08-27 00:06:23 +0200

    Add joint state collector

---
+++ gelataio_boy_control/include/gelataio_boy_control/JointInfoCollector.h
@@ -0,0 +1,14 @@
+//
+// Created by arne on 26.08.19.
+//
+
+#ifndef SRC_JOINTINFOCOLLECTOR_H
+#define SRC_JOINTINFOCOLLECTOR_H
+
+
+class JointInfoCollector {
+
+};
+
+
+#endif //SRC_JOINTINFOCOLLECTOR_H
