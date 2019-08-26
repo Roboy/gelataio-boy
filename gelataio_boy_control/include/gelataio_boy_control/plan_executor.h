6fccf8c2b517a5f0a54a528ac92982a9e356016b
Arne Sachtler <arne.sachtler@gmail.com>
2019-08-27 00:20:05 +0200

    Make cardsflow plan executor independent of group to be moved

--- gelataio_boy_control/include/gelataio_boy_control/plan_executor.h
+++ gelataio_boy_control/include/gelataio_boy_control/plan_executor.h
@@ -13,12 +13,11 @@ public:
 
 class CardsflowPlanExecutor : public plan_executor {
 public:
-    explicit CardsflowPlanExecutor(std::string group_name, ros::NodeHandle *nh);
+    explicit CardsflowPlanExecutor(ros::NodeHandle *nh);
 
     virtual bool executePlan(moveit::planning_interface::MoveGroupInterface::Plan &plan) override;
 
 private:
-    std::string group_name;
     ros::Publisher joint_target_pub;
 };
 
