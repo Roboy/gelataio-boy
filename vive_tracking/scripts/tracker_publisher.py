8fbbd19a3b20bdfd4b12bf3e2ad495506b5af44b
Stanislav Lukyanenko <stlukyanenko@gmail.com>
2019-08-31 19:48:35 +0200

    initial publisher for elbow and writst

--- vive_tracking/scripts/tracker_publisher.py
+++ vive_tracking/scripts/tracker_publisher.py
@@ -62,9 +62,12 @@ def _get_euler_angles_difference(tracker_1_name, tracker_2_name, tracker_1_pose,
     return euler_angles
 
 if __name__ == "__main__":
-    # use these to change publishing behaviour
+    track_elbow = False
+    track_wrist = False
     torso_tracker_name = "tracker_2"
     shoulder_tracker_name = "tracker_3"
+    forearm_tracker_name = "tracker_4"
+    palm_tracker_name = "tracker_5"
 
 
     coefficients = []
@@ -79,43 +82,51 @@ if __name__ == "__main__":
     v = triad_openvr.triad_openvr()
     v.print_discovered_objects()
 
-
     initial_pose_torso = v.devices[torso_tracker_name].get_pose_quaternion()
     initial_pose_shoulder = v.devices[shoulder_tracker_name].get_pose_quaternion()
+    if track_elbow is True:
+        initial_pose_forearm =  v.devices[forearm_tracker_name].get_pose_quaternion()
+    if track_writst is True:
+        initial_pose_palm =  v.devices[palm_tracker_name].get_pose_quaternion()
 
     joint_target_publisher = rospy.Publisher('/joint_targets', JointState, queue_size=1)
-
-
-    #elbow_right_publisher = rospy.Publisher('elbow_right/elbow_right/target', std_msgs.msg.Float32 , queue_size=1)
-
 
     while not rospy.is_shutdown():
         start = time.time()
 
-        euler_12 = _get_euler_angles_difference(torso_tracker_name, shoulder_tracker_name, initial_pose_torso, initial_pose_shoulder)
+        euler_shoulder = _get_euler_angles_difference(torso_tracker_name, shoulder_tracker_name, initial_pose_torso, initial_pose_shoulder)
+        shoulder_axis0 = coefficients[0][0]*euler_shoulder[0] + coefficients[0][1]*euler_shoulder[1] + coefficients[0][2]*euler_shoulder[2] + coefficients[0][6]
+        shoulder_axis1 = coefficients[1][0]*euler_shoulder[0] + coefficients[1][1]*euler_shoulder[1] + coefficients[1][2]*euler_shoulder[2] + coefficients[1][6]
+        shoulder_axis2 = coefficients[2][0]*euler_shoulder[0] + coefficients[2][1]*euler_shoulder[1] + coefficients[2][2]*euler_shoulder[2] + coefficients[2][6]
+        
+        if track_elbow is True:
+            euler_elbow = _get_euler_angles_difference(shoulder_tracker_name, forearm_tracker_name, initial_pose_shoulder, initial_pose_forearm)
+            elbow_axis = coefficients[3][0]*euler_elbow[0] + coefficients[3][1]*euler_elbow[1] + coefficients[3][2]*euler_elbow[2] + coefficients[3][6]
+        else:
+            elbow_axis = 0
+
+        if track_writst is True:
+            euler_wrist = _get_euler_angles_difference(forearm_tracker_name, palm_tracker_name, initial_pose_forearm, initial_pose_palm)
+            wrist_axis = coefficients[3][0]*euler_wrist[0] + coefficients[3][1]*euler_wrist[1] + coefficients[3][2]*euler_wrist[2] + coefficients[3][6]
+        else:
+            wrist_axis = 0
 
 
 
-        print ("Shoulder angles: ", euler_12)
-        #print ("Elbow angles: ", euler_23)
+        print ("Shoulder angles: ", euler_shoulder)
+        print ("Elbow angles: ", euler_23)
 
 
-        shoulder_axis0 = coefficients[0][0]*euler_12[0] + coefficients[0][1]*euler_12[1] + coefficients[0][2]*euler_12[2] + coefficients[0][6]
-        shoulder_axis1 = coefficients[1][0]*euler_12[0] + coefficients[1][1]*euler_12[1] + coefficients[1][2]*euler_12[2] + coefficients[1][6]
-        shoulder_axis2 = coefficients[2][0]*euler_12[0] + coefficients[2][1]*euler_12[1] + coefficients[2][2]*euler_12[2] + coefficients[2][6]
+        
         print ([shoulder_axis0, shoulder_axis1, shoulder_axis2])
-        #elbow_axis = coefficients[3][3]*euler_12[0] + coefficients[3][4]*euler_12[1] + coefficients[3][5]*euler_12[2] + coefficients[3][6]
+        #elbow_axis = coefficients[3][3]*euler_shoulder[0] + coefficients[3][4]*euler_shoulder[1] + coefficients[3][5]*euler_shoulder[2] + coefficients[3][6]
 
 
-        state = JointState(std_msgs.msg.Header(),["shoulder_right_axis0", "shoulder_right_axis1","shoulder_right_axis2"],
-                                           [shoulder_axis0, shoulder_axis1, shoulder_axis2],
-                                           [0,0,0],
-                                           [0,0,0])
+        state = JointState(std_msgs.msg.Header(),
+                                            ["shoulder_right_axis0", "shoulder_right_axis1","shoulder_right_axis2", "elbow_right", "wrist_right"],
+                                            [shoulder_axis0, shoulder_axis1, shoulder_axis2, elbow_axis, wrist_axis],
+                                            [0,0,0,0,0],
+                                            [0,0,0,0,0])
 
         joint_target_publisher.publish(state)
-        #shoulder_right_axis0_publisher.publish(std_msgs.msg.Float32(shoulder_axis0))
-        #shoulder_right_axis1_publisher.publish(std_msgs.msg.Float32(shoulder_axis1))
-        #shoulder_right_axis2_publisher.publish(std_msgs.msg.Float32(shoulder_axis2))
-
-        #elbow_right_publisher.publish(std_msgs.msg.Float32(elbow_axis))
 
