4ecdc622102cccf2e9b7979a39491b81495fb227
Arne Sachtler <arne.sachtler@gmail.com>
2019-09-01 00:39:17 +0200

    Scooping for test script

--- gelataio_boy_control/scripts/test_scooping.sh
+++ gelataio_boy_control/scripts/test_scooping.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rosservice call /scooping_planning/scoop '{start_point: {x: 0, y: -0.5, z: 0.35}}'
+rosservice call /scooping_planning/scoop '{start_point: {x: -0.1, y: -0.4, z: 0.3}}'
 rostopic echo /scooping_planning/status
