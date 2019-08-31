89e65800ea764524b80ea1ed0cbdbca5f023d6f8
Arne Sachtler <arne.sachtler@gmail.com>
2019-08-31 21:54:32 +0200

    Update moveIT config

--- gelataio_boy_control/src/sim_joint_state_converter.cpp
+++ gelataio_boy_control/src/sim_joint_state_converter.cpp
@@ -10,9 +10,25 @@ using namespace std;
 
 static ros::Publisher pub;
 static vector<string> joint_names;
+static bool do_angle_conversion;
 
 void jointStateCallback(const sensor_msgs::JointState &state) {
-    sensor_msgs::JointState s = state;
+    sensor_msgs::JointState s;
+
+    for (int i = 0; i < state.name.size(); i++) {
+        s.name.push_back(state.name[i]);
+        s.velocity.push_back(state.velocity[i]);
+
+        if (do_angle_conversion && (state.name[i] == "wrist_right")) {
+            s.position.push_back(M_PI * state.position[i]/180.0);
+        } else if (do_angle_conversion && (state.name[i] == "elbow_right")) {
+            double angle = state.position[i];
+            if (angle > M_PI) angle -= 2*M_PI;
+            s.position.push_back(angle);
+        } else {
+            s.position.push_back(state.position[i]);
+        }
+    }
     s.header.stamp = ros::Time::now();
     pub.publish(s);
     ROS_INFO_THROTTLE(10.0, "I'm alive.");
@@ -22,6 +38,8 @@ int main(int argc, char **argv) {
 
     ros::init(argc, argv, "sim_joint_state_converter");
     ros::NodeHandle nh;
+
+    nh.getParam("do_angle_conversion", do_angle_conversion);
 
     if (ros::param::has("joint_names")) {
         auto sub = nh.subscribe("/cardsflow_joint_states", 10, jointStateCallback);
