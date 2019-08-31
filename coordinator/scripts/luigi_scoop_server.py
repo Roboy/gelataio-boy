fa5ed821bb5e46970aaf59254f17ec05eddb93a3
Mohmad <no.future2e1@gmail.com>
2019-08-31 17:25:34 +0200

    call go home service after finishing scooping

--- coordinator/scripts/luigi_scoop_server.py
+++ coordinator/scripts/luigi_scoop_server.py
@@ -5,6 +5,7 @@ roslib.load_manifest('coordinator')
 import rospy
 import actionlib
 from std_msgs.msg import *
+from std_srvs.srv import Trigger, TriggerRequest
 import sched, time
 
 # Imported msgs are OrderIceCreamAction, OrderIceCreamActionFeedback
@@ -49,6 +50,22 @@ class ScoopServer:
 
   def ScoopStatusCallback(self, scooping_status):
     self.scooping_status_ = scooping_status
+
+  # Arguments are of Pose type
+  def GoHome(self):
+    rospy.wait_for_service('scooping_planning/go_home')
+    try:
+      # TODO: Only call this when status is IDLE
+      goHome = rospy.ServiceProxy('scooping_planning/go_home', Trigger)
+
+      # Create an object of the type TriggerRequest. We nned a TriggerRequest for a Trigger service
+      req = TriggerRequest()
+
+      # Now send the request through the connection
+      resp = goHome(req)
+      return resp.success
+    except rospy.ServiceException, e:
+      print "Service call failed: %s"%e
 
   # Arguments are of Pose type
   def TranslationalPTPMotionClient(self, startPosition, endPosition):
@@ -177,6 +194,13 @@ class ScoopServer:
           #   # call scooping service with the points
           #   #
       self._feedback.finished_scoops[scoop] = 1
+
+    wentHome = self.GoHome()
+
+    if wentHome:
+      rospy.loginfo('Went home')
+    else:
+      rospy.logwarn('Could not go home, the traffic is terrible')
 
     if success:
       self._result.success = True
