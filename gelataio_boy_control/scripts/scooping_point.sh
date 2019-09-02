8aff288b25ef81205c12d7804c8fe32333d80d9d
Arne Sachtler <arne.sachtler@gmail.com>
2019-09-02 23:00:59 +0200

    Add two well known poses

---
+++ gelataio_boy_control/scripts/scooping_point.sh
@@ -0,0 +1,1 @@
+rostopic pub /joint_targets sensor_msgs/JointState "{name: [shoulder_right_axis0, shoulder_right_axis1, shoulder_right_axis2, elbow_right], position:[-1.57, 0.4, 1.7, 1.8], velocity:[0.0]}"
