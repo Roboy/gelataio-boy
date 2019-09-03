a60dc15a738e88921ecc45125bca936df5bb82f4
Stanislav Lukyanenko <stlukyanenko@gmail.com>
2019-09-03 19:18:17 +0200

    Initial rework to do arm control through IK

--- vive_tracking/scripts/tracker_publisher.py
+++ vive_tracking/scripts/tracker_publisher.py
@@ -5,143 +5,74 @@ import time
 import rospy
 import numpy as np
 import math
-from pyquaternion import Quaternion
 import std_msgs
 from sensor_msgs.msg import JointState
+import roboy_middleware_msgs.srv 
+import warnings
 import csv
 import pickle
 from scipy.spatial.transform import Rotation as R
 
 rospy.init_node('tracker_tf_broadcaster')
 
-# Checks if a matrix is a valid rotation matrix.
-def isRotationMatrix(R) :
-    Rt = np.transpose(R)
-    shouldBeIdentity = np.dot(Rt, R)
-    I = np.identity(3, dtype = R.dtype)
-    n = np.linalg.norm(I - shouldBeIdentity)
-    return n < 1e-6
+def _get_tracker_coordinates(tracker_name):
+    pose_matrix = v.devices[controller_name].get_pose_matrix()
+    x = pose_mat[0][3]
+    y = pose_mat[1][3]
+    z = pose_mat[2][3]
+    return np.array([x,y,z])
 
-
-# Calculates rotation matrix to euler angles
-def rotationMatrixToEulerAngles(R) :
-
-    assert(isRotationMatrix(R))
-
-    sy = math.sqrt(R[0,0] * R[0,0] +  R[1,0] * R[1,0])
-
-    singular = sy < 1e-6
-
-    if  not singular :
-        z = math.atan2(R[2,1] , R[2,2])
-        y = math.atan2(-R[2,0], sy)
-        x = math.atan2(R[1,0], R[0,0])
-    else :
-        z = math.atan2(-R[1,2], R[1,1])
-        y = math.atan2(-R[2,0], sy)
-        x = 0
-
-    return np.array([x, y, z])
-
-
-
-def get_quarterion(device_name, pose):
-    try:
-        pose = v.devices[device_name].get_pose_quaternion()
-    except:
-        rospy.loginfo("could not find transform world->"+ device_name +", initialization might be wrong")
-
-    q_tracker = Quaternion(pose[6],pose[3],pose[4],pose[5])                 #*q_init1.inverse
-    return q_tracker
-
-def _get_euler_angles_difference(tracker_1_name, tracker_2_name, tracker_1_pose, tracker_2_pose):
-    q_tracker_1 = get_quarterion(tracker_1_name, tracker_1_pose)
-    q_tracker_2 = get_quarterion(tracker_2_name, tracker_2_pose)
-
-    q_tracker_diff = q_tracker_2*q_tracker_1.inverse
-
-    euler_angles = rotationMatrixToEulerAngles(q_tracker_diff.rotation_matrix)
-
-    return euler_angles
+def _get_endeffector_coordinats(endeffector_name, topic_name):
+    endeffector_coordinates = np.zeros(3)
+    while endeffector_coordinates == np.zeros():
+        p = rospy.wait_for_message(topic_name, PoseStamped)
+        if p.header.frame_id == endeffector_name:
+            endeffector_coordinates[0] = p.pose.position.x
+            endeffector_coordinates[1] = p.pose.position.x
+            endeffector_coordinates[2] = p.pose.position.x
+    return endeffector_coordinates
 
 if __name__ == "__main__":
-    track_elbow = False
-    track_wrist = False
-    torso_tracker_name = "tracker_1"
-    shoulder_tracker_name = "tracker_2"
-    forearm_tracker_name = "tracker_3"
-    palm_tracker_name = "tracker_5"
+    controller_name = "controller_1"
+    endeffector_name = "scooper"
+    inverse_kinematics_service = "\ik"
+    robot_state_topic = "robot_state"
+    target_joint_angle_topic = "joint_targets"
+    
 
+    rospy.init_node('vive_to_ik_publisher')
+    angle_publisher = rospy.Publisher('/joint_targets', JointState, queue_size=5)
+        
 
-    coefficients = []
-    csvfile =  open('calibration.csv', 'r')
-    reader = csv.reader(csvfile)
-
-    for row in reader:
-        r = np.array([float(i) for i in row])
-        coefficients.append(r)
-
-
-    v = triad_openvr.triad_openvr()
-    v.print_discovered_objects()
-
-    initial_pose_torso = v.devices[torso_tracker_name].get_pose_quaternion()
-    initial_pose_shoulder = v.devices[shoulder_tracker_name].get_pose_quaternion()
-    if track_elbow is True:
-        initial_pose_forearm =  v.devices[forearm_tracker_name].get_pose_quaternion()
-    if track_wrist is True:
-        initial_pose_palm =  v.devices[palm_tracker_name].get_pose_quaternion()
-
-    joint_target_publisher = rospy.Publisher('/joint_targets', JointState, queue_size=1)
-    with open("calibration_file", 'rb') as pickle_file:
-        reg_shoulder = pickle.load(pickle_file)
+    initial_position_controller = _get_position_coordinates(controller_name)
+    initial_position_scooper = _get_endeffector_coordinats(endeffector_name, robot_state_topic)
 
     while not rospy.is_shutdown():
         start = time.time()
 
-        #print (coefficients[0][0], coefficients[0][1], coefficients[0][2], coefficients[0][9])
-        euler_shoulder = _get_euler_angles_difference(torso_tracker_name, shoulder_tracker_name, initial_pose_torso, initial_pose_shoulder)
-        #shoulder_axis0 = coefficients[0][0]*euler_shoulder[0] - coefficients[0][1]*euler_shoulder[1] + coefficients[0][2]*euler_shoulder[2] + coefficients[0][9]
-        #shoulder_axis1 = coefficients[1][0]*euler_shoulder[0] - coefficients[1][1]*euler_shoulder[1] + coefficients[1][2]*euler_shoulder[2] + coefficients[1][9]
-        #shoulder_axis2 = coefficients[2][0]*euler_shoulder[0] - coefficients[2][1]*euler_shoulder[1] + coefficients[2][2]*euler_shoulder[2] + coefficients[2][9]
+        current_position_controller = _get_position_coordinates(controller_name)
+        delta_position_controller = current_position_controller - initial_position_controller
         
-        mat = R.from_euler('xyz', euler_shoulder).as_dcm()
-        pred = reg_shoulder.predict([mat.flatten()])
-        predicted_mat = np.reshape(pred, (3,3))
-        pred_angles = R.from_dcm(predicted_mat).as_euler('xyz')
+        modified_position_scooper = initial_position_scooper + delta_position_controller        
 
-        shoulder_axis0 = pred_angles[0]
-        shoulder_axis1 = pred_angles[1]
-        shoulder_axis2 = pred_angles[2]
+        rospy.wait_for_service(inverse_kinematics_service)
+        get_ik = rospy.ServiceProxy(inverse_kinematics_service, roboy_middleware_msgs.srv.InverseKinematics)
 
-        if track_elbow is True:
-            euler_elbow = _get_euler_angles_difference(shoulder_tracker_name, forearm_tracker_name, initial_pose_shoulder, initial_pose_forearm)
-            elbow_axis = coefficients[3][0]*euler_elbow[0] + coefficients[3][1]*euler_elbow[1] + coefficients[3][2]*euler_elbow[2] + coefficients[3][6]
-        else:
-            elbow_axis = 0
+        try:
 
-        if track_wrist is True:
-            euler_wrist = _get_euler_angles_difference(forearm_tracker_name, palm_tracker_name, initial_pose_forearm, initial_pose_palm)
-            wrist_axis = coefficients[3][0]*euler_wrist[0] + coefficients[3][1]*euler_wrist[1] + coefficients[3][2]*euler_wrist[2] + coefficients[3][6]
-        else:
-            wrist_axis = 0
+            requested_pose = Pose(position = modified_position_scooper)
+            it_request = roboy_middleware_msgs.srv.InverseKinematicsRequest(endeffector_name, 0, endeffector_name, requested_pose)
+            ik = get_ik(it_request)
 
-
-
-        print ("Shoulder angles: ", euler_shoulder)
-        #print ("Elbow angles: ", euler_23)
-
-
+            joint_state = JointState([],[],[],[])
+            for i in range(len(ik.joint_names)):
+                joint_state.name.append(ik.joint_names[i])
+                joint_state.position.append(ik.angles[i])
+                joint_state.velocity.append(0)
+                joint_state.effort.append(0)
+            angle_publisher.publish(joint_state)
         
-        print ([shoulder_axis0, shoulder_axis1, shoulder_axis2])
-        #elbow_axis = coefficients[3][3]*euler_shoulder[0] + coefficients[3][4]*euler_shoulder[1] + coefficients[3][5]*euler_shoulder[2] + coefficients[3][6]
-
-
-        state = JointState(std_msgs.msg.Header(),
-                                            ["shoulder_right_axis0", "shoulder_right_axis1","shoulder_right_axis2", "elbow_right", "wrist_right"],
-                                            [shoulder_axis0, shoulder_axis1, shoulder_axis2, elbow_axis, wrist_axis],
-                                            [0,0,0,0,0],
-                                            [0,0,0,0,0])
-
-        joint_target_publisher.publish(state)
+        except rospy.ServiceException as exc:
+            warnings.warn("Inverse Kinematics calculation wasn't successfull\n: " + str(exc))
+        
 
