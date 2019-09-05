07ca471a260a412727c7cb70613487be56b08b32
missxa <aliona.kharchenko@gmail.com>
2019-09-05 05:13:24 +0200

    when trigger is pressed the palm right current position is used as the origin of the controller, and will diff from there

--- vive_tracking/scripts/vive_to_ik_control.py
+++ vive_tracking/scripts/vive_to_ik_control.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import triad_openvr
 import time
@@ -7,34 +7,33 @@ import numpy as np
 import math
 import std_msgs
 from sensor_msgs.msg import JointState
+from std_srvs.srv import SetBool
 from geometry_msgs.msg import PoseStamped
 from geometry_msgs.msg import Pose
 from geometry_msgs.msg import Point
 from std_msgs.msg import Header
-import roboy_middleware_msgs.srv 
+from roboy_middleware_msgs.msg import MotorCommand
+import roboy_middleware_msgs.srv
+from geometry_msgs.msg import PoseStamped
 import time
 import csv
 import pickle
 from scipy.spatial.transform import Rotation as R
+import keyboard
 
-#Getting triad_openvr object with necessary methods to other functions and checks whether vive controller is connected
-def _get_triadVR_object(controller_name):
-    v = triad_openvr.triad_openvr()
-    while controller_name not in v.devices:
-        print("Controller not connected or turned off, waiting 5 seconds to reconnect ")
-        print(v.devices)
-        time.sleep(5)
-    return v
+v = triad_openvr.triad_openvr()
+take_control = False
+link_coordinates = [0,0,0]
+palm_pose = [0,0,0]
 
 #Getting 3d coordinates of the vive controller with respect to the lighthouses
 def _get_tracker_coordinates(controller_name):
-    v = _get_triadVR_object(controller_name)
+    global v
     pose_matrix = v.devices[controller_name].get_pose_matrix()
     print(pose_matrix)
-    while pose_matrix is None:
-        print("Controller is out of lighthouses reach, waiting for controller ")
-        time.sleep(1)
-        pose_matrix = v.devices[controller_name].get_pose_matrix()
+    if pose_matrix is None:
+        return [0,0,0]
+    pose_matrix = v.devices[controller_name].get_pose_matrix()
     x = pose_matrix[0][3]
     y = pose_matrix[1][3]
     z = pose_matrix[2][3]
@@ -42,12 +41,12 @@ def _get_tracker_coordinates(controller_name):
 
 #Detects the usage of the vertical axis of the trackpad controller
 def _get_trackpad_movement(controller_name):
-    v = _get_triadVR_object(controller_name)
+    global v
     return v.devices[controller_name].get_controller_inputs()['trackpad_y']
 
 #Detects the press of the trigger
 def _if_trigger_pressed(controller_name):
-    v = _get_triadVR_object(controller_name)
+    global v
     return v.devices[controller_name].get_controller_inputs()['trigger'] > 0.5
 
 #Return the curren angle of a particular robot joint
@@ -61,7 +60,7 @@ def _get_link_coordinats(link_name, topic_name):
     link_coordinates = np.zeros(3)
     
     def pose_callback(p):
-        nonlocal link_coordinates
+        global link_coordinates
         if p.header.frame_id == endeffector_name:
             link_coordinates[1] = p.pose.position.y
             link_coordinates[2] = p.pose.position.z
@@ -76,7 +75,7 @@ def _get_link_coordinats(link_name, topic_name):
    
 #Creates and returns a JointState object, which includes names, positions. velocities and efforts necessary for every joint
 #Adds wrist separately since it's controlled directly
-def _get_joint_state(names, angles, wrist_name, wrist_angle):
+def _get_joint_state(names, angles, wrist_name):
     joint_state = JointState(Header(),[],[],[],[])
     for i in range(len(names)):
         if names[i] != wrist_name:
@@ -85,10 +84,6 @@ def _get_joint_state(names, angles, wrist_name, wrist_angle):
             joint_state.position.append(angles[i])
             joint_state.velocity.append(0)
             joint_state.effort.append(0)
-    joint_state.name.append(wrist_name)
-    joint_state.position.append(wrist_angle)
-    joint_state.velocity.append(0)
-    joint_state.effort.append(0)
 
     print(joint_state)
     return joint_state
@@ -98,7 +93,20 @@ def _get_default_joint_state(topic_name, wrist_name):
     msg = rospy.wait_for_message(topic_name, JointState)
     angles = [1.57 if i == "elbow_right" else 0 for i in msg.name]
 
-    return _get_joint_state(msg.name, angles, wrist_name, 0)
+    return _get_joint_state(msg.name, angles, wrist_name)
+
+def take_control_srv(req):
+    global take_control
+    take_control = req.data
+    if take_control:
+        return True, "taking control"
+    else:
+        return True, "giving control back"
+
+def pose_cb(data):
+    if data.header.frame_id == "palm_right":
+        global palm_pose
+        palm_pose = [data.pose.position.x, data.pose.position.y, data.pose.position.z]
 
 if __name__ == "__main__":
     controller_name = "controller_1"
@@ -108,52 +116,95 @@ if __name__ == "__main__":
     target_joint_angle_topic = "joint_targets"
     wrist_axis_name = "wrist_right"
     joint_state_topic_name =  "cardsflow_joint_states"
-
+    global v
+    global palm_pose
     rospy.init_node('vive_to_ik_publisher')
+    requested_position_topic_name =  "/vive/requested_position"
+    vive_joint_state_topic_name = '/vive/ik_joint_state'
+    
     angle_publisher = rospy.Publisher('/joint_targets', JointState, queue_size=5)
-        
-    joint_state = _get_default_joint_state(joint_state_topic_name, wrist_axis_name)
-    angle_publisher.publish(joint_state)
-
-    initial_position_controller = _get_tracker_coordinates(controller_name)
-    initial_position_scooper = _get_link_coordinats(endeffector_name, robot_state_topic)
+    s = rospy.Service('take_control', SetBool, take_control_srv)
 
     joint_state = _get_default_joint_state(joint_state_topic_name, wrist_axis_name)
+    
+    wrist_publisher = rospy.Publisher('roboy/middleware/MotorCommand', MotorCommand)
+    wrist_angle = 0.2
 
+    robot_state_sub = rospy.Subscriber('/robot_state', PoseStamped, pose_cb)
+    initial_position_scooper = [0.5,0,0]
+    initial_position_controller = [0,0,0]
+    modified_position_scooper_prev = [0,0,0]
+
+    t0 = rospy.Time.now()
+    wrist_angle_prev = 0.2
     while not rospy.is_shutdown():
-        if _if_trigger_pressed(controller_name):
-            joint_state = _get_default_joint_state(joint_state_topic_name, wrist_axis_name)
-            initial_position_controller = _get_tracker_coordinates(controller_name)
-            initial_position_scooper = _get_link_coordinats(endeffector_name, robot_state_topic)
+        if(take_control):
+            if((rospy.Time.now()-t0).to_sec()>0.1):
+                t0 = rospy.Time.now()
+                wrist_angle = (v.devices[controller_name].get_controller_inputs()['trackpad_y']+1)/2
+                wrist_angle = wrist_angle*180
+                if(wrist_angle<20):
+                    wrist_angle = 20
+                if(wrist_angle>160):
+                    wrist_angle = 160
+                if(wrist_angle_prev is not wrist_angle):
+                    rospy.loginfo_throttle(1,wrist_angle)
+                    wrist_publisher.publish(MotorCommand(6, [2], [wrist_angle]))
+                    wrist_angle_prev = wrist_angle
 
+
+            if v.devices[controller_name].get_controller_inputs()['trigger']>0.5:
+                pose_matrix = v.devices[controller_name].get_pose_matrix()
+                if pose_matrix is None:
+                    initial_position_scooper = [0.5,0,0]
+                    continue
+                initial_position_controller = [pose_matrix[0][3],pose_matrix[1][3],pose_matrix[2][3]]
+                initial_position_scooper = palm_pose
+                rospy.loginfo_throttle(0.1,initial_position_scooper)
+                # rospy.loginfo_throttle(0.1,palm_pose)
+            else:
+                pose_matrix = v.devices[controller_name].get_pose_matrix()
+                rospy.loginfo_throttle(1,pose_matrix)
+                if pose_matrix is None:
+                    continue
+                x = pose_matrix[0][3]
+                y = pose_matrix[1][3]
+                z = pose_matrix[2][3]
+
+                current_position_controller = np.array([x,y,z])
+                diff_controller = current_position_controller-initial_position_controller
+                modified_position_scooper = [initial_position_scooper[0]+diff_controller[2],initial_position_scooper[1]+diff_controller[0],initial_position_scooper[2]+diff_controller[1]]
+
+                new_pos = [0.6*modified_position_scooper_prev[0]+ 0.4*modified_position_scooper[0],
+                                             0.6*modified_position_scooper_prev[1]+ 0.4*modified_position_scooper[1],
+                                             0.6*modified_position_scooper_prev[2]+ 0.4*modified_position_scooper[2]]
+                modified_position_scooper_prev = modified_position_scooper
+
+                rospy.loginfo_throttle(0.1,new_pos)
+
+                pos = Point(new_pos[0], new_pos[1], new_pos[2])
+                requested_pose = Pose(position = pos)
+                ik_request = roboy_middleware_msgs.srv.InverseKinematicsRequest(endeffector_name, 1, endeffector_name, requested_pose)
+
+                try:
+                    get_ik = rospy.ServiceProxy(inverse_kinematics_service, roboy_middleware_msgs.srv.InverseKinematics)
+                    ik = get_ik(ik_request);
+                    joint_state = _get_joint_state(ik.joint_names, ik.angles, wrist_axis_name)
+                except rospy.ServiceException as exc:
+                    print("Inverse Kinematics calculation wasn't successfull\n: " + str(exc))
+
+                angle_publisher.publish(joint_state)
         else:
-            current_position_controller = _get_tracker_coordinates(controller_name)
-            delta_position_controller = current_position_controller - initial_position_controller
-            
-            #Rewriting the coordinates from OpenVR's to ROS's coordinate frames
-            delta_position_scooper = np.array([
-                delta_position_controller[0],
-                -delta_position_controller[2],
-                delta_position_controller[1]
-            ])
-            
-            modified_position_scooper = initial_position_scooper + delta_position_scooper  
-            
-            
-            wrist_angle = _get_single_joint_angle(wrist_axis_name, joint_state_topic_name) +  _get_trackpad_movement(controller_name)
-            
-            pos = Point(modified_position_scooper[0], modified_position_scooper[1], modified_position_scooper[2])
-            requested_pose = Pose(position = pos)
-            ik_request = roboy_middleware_msgs.srv.InverseKinematicsRequest(endeffector_name, 1, endeffector_name, requested_pose)
-            
-            try:
-                get_ik = rospy.ServiceProxy(inverse_kinematics_service, roboy_middleware_msgs.srv.InverseKinematics)
-                ik = get_ik(ik_request)
-                joint_state = _get_joint_state(ik.joint_names, ik.angles, wrist_axis_name, wrist_angle)
-            except rospy.ServiceException as exc:
-                print("Inverse Kinematics calculation wasn't successfull\n: " + str(exc))
+            rospy.loginfo_throttle(5,"its really not in my hands")
+        # if keyboard.is_pressed('s'):  # if key 's' is pressed
+        #     take_control = not take_control
+        #     if(take_control):
+        #         print('taking control')
+        #     else:
+        #         print('giving control back')
+        # else:
+        #     print('press s or go to hell')
 
-        angle_publisher.publish(joint_state)
 
             
         
